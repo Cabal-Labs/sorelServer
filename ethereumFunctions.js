2a47d0fc3d0050e7203fb9dcf8e2d3f700ef555b
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-10-19 14:46:16 -0400

    s

--- ethereumFunctions.js
+++ ethereumFunctions.js
@@ -7,24 +7,16 @@ import {
 } from 'ethereumjs-util';
 import crypto from 'crypto';
 
-
-
-
 function getRandomBytes(length) {
-    const randomBytes = new Uint8Array(length);
-    crypto.getRandomValues(randomBytes);
-    return randomBytes;
+    return crypto.randomBytes(length);
 }
 
 function getCompressedPublicKey(publicKey) {
-  
     const x = publicKey.slice(1, 33);
     const y = publicKey.slice(33, 65);
-
     const prefix = y[y.length - 1] % 2 === 0 ? '02' : '03';
     return prefix + x.toString('hex');
 }
-
 
 async function personalSign(message, privateKey) {
     const messageHash = keccakFromString(`\x19Ethereum Signed Message:\n${message.length}${message}`, 256);
@@ -36,10 +28,8 @@ async function doubleSignMessageEth(aPrivKey, wPrivKey, message) {
     const _message = Buffer.from(message);
     const avatarPrivateKey = Buffer.from(aPrivKey, 'hex');
     const walletPrivateKey = Buffer.from(wPrivKey, 'hex');
-
     const avatarSignature = await personalSign(_message, avatarPrivateKey);
     const walletSignature = await personalSign(_message, walletPrivateKey);
-
     return [avatarSignature, walletSignature];
 }
 
@@ -47,27 +37,19 @@ async function signMessageEth(aPrivKey, message){
     const _message = Buffer.from(message);
     const avatarPrivateKey = Buffer.from(aPrivKey, 'hex');
     const avatarSignature = await personalSign(_message, avatarPrivateKey);
-
     return avatarSignature;
 }
 
-
 function generateKeyPair() {
-    
     const privateKey = getRandomBytes(32);
-
-
     const publicKeyFull = privateToPublic(privateKey);
     const publicKeyCompressed = getCompressedPublicKey(publicKeyFull);
-
     const privateKeyHex = bufferToHex(privateKey).slice(2);
-
     return {
         privateKey: privateKeyHex,
         publicKey: publicKeyCompressed
     };
 }
-
 
 export {
     generateKeyPair,
