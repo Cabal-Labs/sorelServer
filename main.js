fd0c85e34607fbb8923c9ae074591ae0a5dfb9a1
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-10-19 19:18:16 -0400

    s

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ app.post('/sign-message', async (req, res) => {
     }
 });
 
-const PORT = 8000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
